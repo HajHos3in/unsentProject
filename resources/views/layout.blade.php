0a4ac7b1f540aa2797a3b51a78bed769d0818156
HajHos3in <hosseinahmad54@yahoo.com>
2024-04-12 16:00:50 +0330

    write terms

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -66,10 +66,8 @@
                     <div class="row d-flex justify-content-center">
                         <div class="col-lg-8">
                             <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-                                distinctio earum repellat quaerat voluptatibus placeat nam,
-                                commodi optio pariatur est quia magnam eum harum corrupti
-                                dicta, aliquam sequi voluptate quas.
+                                «حرفای ناگفته» فضایی امن و حمایتی برای به اشتراک گذاشتن نوشته های عاشقانه و درام به صورت ناشناس است. این نوشته ها می توانند شامل نامه ها، اعترافات، اشعار، داستان های کوتاه و هر نوع نوشته ی دیگری باشند که بیانگر احساسات عمیق شما باشد.<br>
+                                الگو گرفته از وبسایت theunsentproject.com
                             </p>
                         </div>
                     </div>
